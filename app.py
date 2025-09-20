d9451d00b9a3483b1d1d2568d7d8564d9450cee6
Jam <jameskuyt655@gmail.com>
2025-09-20 16:57:22 +0000

    edit app,py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import webview
 
 # Create the window that loads your HTML file
-webview.create_window("My App Window", url='index.html')
+webview.create_window("catchat-meow", url='index.html')
 
 # Start the application. This will open the window.
 webview.start()
