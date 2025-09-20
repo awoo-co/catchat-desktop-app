9075c5f5b686d8a35551642d33c9e09ff0322f28
Jam <jameskuyt655@gmail.com>
2025-09-20 16:46:44 +0000

    first commit

---
+++ app.py
@@ -0,0 +1,7 @@
+import webview
+
+# Create the window that loads your HTML file
+webview.create_window("My App Window", url='index.html')
+
+# Start the application. This will open the window.
+webview.start()
